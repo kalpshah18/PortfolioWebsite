9108a5d20a02ac6ba246dce689ee46adead1e292
Kalp Shah <shahkalp07@gmail.com>
2024-12-04 18:00:06 +0530

    Added website

---
+++ src/content_option.js
@@ -0,0 +1,96 @@
+const logotext = "KALP";
+const meta = {
+    title: "Kalp Shah",
+    description: "I’m Kalp Shah - a Data Scientist, currently studying at IIT Gandhinagar",
+};
+
+const introdata = {
+    title: "I’m Kalp Shah",
+    animated: {
+        first: "I turn coffee into code",
+        second: "I code cool websites",
+        third: "I develop amazing algorithms",
+    },
+    description: "Student at Indian Institute of Technology, Gandhinagar. Interested in Artificial Intelligence and Data Science.",
+    your_img_url: "https://i.gifer.com/RQt6.gif",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "Python developer with a passion for crafting efficient automation solutions and building intelligent systems. Currently Studying at Indian Institute of Technology, Gandhinagar. Let's connect to explore how I can contribute to your project's success!",
+};
+const worktimeline = [{
+        jobtitle: "Entrepreneurship Initiative, IIT Gandhinagar",
+        where: "Gandhinagar, India",
+        date: "November 2024 - Present",
+    },
+    {
+        jobtitle: "Amalthea, IIT Gandhinagar",
+        where: "Gandhinagar, India",
+        date: "August 2024 - Present",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 95,
+    },
+    {
+        name: "Node.Js",
+        value: 85,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Automation",
+        description: "Automate Tasks with Python Libraries such as Selenium and Beautiful Soup.",
+    },
+    {
+        title: "Websites",
+        description: "Build Websites with Latest Web Technologies like React.Js and Next.Js",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://media.istockphoto.com/id/498731269/photo/black-and-white-dj-and-crowd-in-nightclub.jpg?s=612x612&w=0&k=20&c=XE3Bo5LRCBLQhUmDy-A_wOAPJIL2_Gpj0ZFN04aO5Ro=",
+        description: "Concert Website",
+        link: "https://github.com/kalpshah18/concertWebsite",
+    },
+    {
+        img: "https://i.postimg.cc/prYz1sDT/indeed-grayscale.png",
+        description: "Indeed Job Scraper with Python",
+        link: "https://github.com/kalpshah18/pyIndeedScraper",
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZiZbUE65YZ6L1r6fIkEjQXzTtlLq1WJvRDg&s",
+        description: "Python Script for Calculating Amount of Stocks Required for creating an Equal Weight S&P 500 Fund",
+        link: "https://github.com/kalpshah18/S-P500IndexFund",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kalpshah1808@gmail.com",
+    YOUR_FONE: "(+91)6355258281",
+};
+
+const socialprofils = {
+    github: "https://github.com/kalpshah18",
+    linkedin: "https://www.linkedin.com/in/kalpshah18/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/contact/index.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import * as emailjs from "emailjs-com";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { meta } from "../../content_option";
+import { Container, Row, Col, Alert } from "react-bootstrap";
+import { contactConfig } from "../../content_option";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const ContactUs = () => {
+  const [formData, setFormdata] = useState({
+    email: "",
+    name: "",
+    message: "",
+    loading: false,
+    show: false,
+    alertmessage: "",
+    variant: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormdata({ loading: true });
+
+    const templateParams = {
+      from_name: formData.email,
+      user_name: formData.name,
+      to_name: contactConfig.YOUR_EMAIL,
+      message: formData.message,
+    };
+
+    emailjs
+      .send(
+        contactConfig.YOUR_SERVICE_ID,
+        contactConfig.YOUR_TEMPLATE_ID,
+        templateParams,
+        contactConfig.YOUR_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setFormdata({
+            loading: false,
+            alertmessage: "SUCCESS! Thank you for your message",
+            variant: "success",
+            show: true,
+          });
+        },
+        (error) => {
+          console.log(error.text);
+          setFormdata({
+            alertmessage: `Failed to send! ${error.text}`,
+            variant: "danger",
+            show: true,
+          });
+        }
+      );
+  };
+
+  const handleChange = (e) => {
+    setFormdata({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <HelmetProvider>
+      <Container>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{meta.title} | Contact</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">Contact Me</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5" className="mb-5">
+            <address>
+              <strong>Email:</strong>{" "}
+              <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
+                {contactConfig.YOUR_EMAIL}
+              </a>
+              <br />
+              <br />
+              {contactConfig.hasOwnProperty("YOUR_FONE") ? (
+                <p>
+                  <strong>Phone:</strong> {contactConfig.YOUR_FONE}
+                </p>
+              ) : (
+                ""
+              )}
+            </address>
+            <strong>Socials:</strong>{" "}
+            <div className="social-links">
+              <a href={socialprofils.github} target="_blank" rel="noopener noreferrer">
+                <FaGithub size={30} className="m-2" />
+              </a>
+              <a href={socialprofils.linkedin} target="_blank" rel="noopener noreferrer">
+                <FaLinkedin size={30} className="m-2" />
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <div className={formData.loading ? "loading-bar" : "d-none"}></div>
+    </HelmetProvider>
+  );
+};
